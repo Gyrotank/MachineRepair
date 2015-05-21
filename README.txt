680753bd93c2d50103496c44f8972528cca6ff5b
Gyrotank <glomozda.dm@gmail.com>
2015-05-22 00:31:39 +0300

    pom.xml maven deployment is now available

---
+++ README.txt
@@ -0,0 +1,72 @@
+DEPLOYING AND RUNNING THE PROJECT
+---------------------------------
+
+In order to run the project, you will need:
+
+1. Web server: Tomcat 7,8
+2. JRE 1.8
+3. Database server (MySQL recommended)
+4. Maven 3.2.2
+
+Steps to deploy and run the project:
+1. Download the project and unzip it
+
+2. Prepare Tomcat application server
+2.1. Open file CATALINA_HOME/conf/tomcat-users.xml
+2.2. If you haven't already, create the user with roles 'manager-gui' and 'manager-script'. This is done by adding 
+the required information to the block designated by <tomcat-users>...</tomcat-users> tags.
+For example:
+<tomcat-users ...>
+	<role rolename="manager-script" />
+	<role rolename="manager-gui" />
+	<user username="manager" password="pass" roles="manager-gui,manager-script" />
+</tomcat_users>
+2.3. Save the file
+
+3. Preparation of Maven 
+3.1. Open the file MAVEN_HOME/conf/settings.xml
+3.2. Add information about your Tomcat server inside the <servers>...</servers> tags. For example:
+  <servers>
+  ...
+  	<server>
+		<id>localhost-Tomcat7</id>
+		<username>maven</username>
+		<password>maven</password>
+	</server>
+  </servers>
+3.3. Save the file.
+
+4. Preparation of pom.xml
+4.1. In pom.xml find the following block of properties and set the correct parameters 
+corresponding to your web server and database settings.
+For example:
+	<properties>
+		<db.name>machinerepair</db.name>
+		<db.driver>com.mysql.jdbc.Driver</db.driver>
+		<db.url>jdbc:mysql://localhost/</db.url>
+		<db.url.name>${db.url}${db.name}</db.url.name> 
+		<db.username>root</db.username>
+		<db.password>root</db.password>
+		<db.dialect>org.hibernate.dialect.MySQLDialect</db.dialect>
+		<tomcat.url>http://localhost:8080</tomcat.url>
+		<tomcat.deploy.url>${tomcat.url}/manager/text</tomcat.deploy.url>
+		<tomcat.deploy.server>localhost-Tomcat7</tomcat.deploy.server>
+	</properties>
+4.2. Save this file
+
+5. Start Tomcat 
+
+6. Go to the root folder of the project
+
+7. Run maven using the command: 
+mvn db:drop db:create db:schema db:data clean tomcat7:deploy
+
+This command will drop the database if it exists, create the database if it doesn't exist, create 
+the database tables and fill them with the data according to SQL scripts located in folders specified in
+<dbSchemaScriptsDirectory> and <dbDataScriptsDirectory> tags respectfully.
+
+8. If you need to fully redeploy the project, use next command:
+mvn db:drop db:create db:schema db:data clean tomcat7:redeploy
+
+9. If you don't need or want to reinitialize the DB, use the following command:
+mvn clean tomcat7:redeploy
---
+++ src/main/sql/schema/createtables.sql
@@ -0,0 +1,95 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `clients_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_id` bigint(11) NOT NULL,
+  PRIMARY KEY (`clients_id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `user_id` (`user_id`),
+  KEY `user_id_2` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=101 ;
+
+CREATE TABLE IF NOT EXISTS `machines` (
+  `machines_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `machine_serviceable_id` bigint(11) NOT NULL,
+  `serial_number` varchar(32) NOT NULL,
+  `year` int(4) NOT NULL,
+  `times_repaired` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`machines_id`),
+  UNIQUE KEY `serial_number` (`serial_number`),
+  KEY `machine_serviceable_id` (`machine_serviceable_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+
+CREATE TABLE IF NOT EXISTS `machines_serviceable` (
+  `machines_serviceable_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `trademark` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `country_ru` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `available` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`machines_serviceable_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `orders_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `client_id` bigint(11) NOT NULL,
+  `repair_type_id` bigint(11) NOT NULL,
+  `machine_id` bigint(11) NOT NULL,
+  `status_id` bigint(11) NOT NULL,
+  `start` date NOT NULL,
+  `manager` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '-',
+  PRIMARY KEY (`orders_id`),
+  KEY `user_id` (`client_id`),
+  KEY `repair_type_id` (`repair_type_id`,`machine_id`),
+  KEY `machine_id` (`machine_id`),
+  KEY `client_id` (`client_id`),
+  KEY `status_id` (`status_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=38 ;
+
+CREATE TABLE IF NOT EXISTS `order_statuses` (
+  `order_statuses_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `order_status_number` int(3) NOT NULL,
+  `order_status_name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `order_status_name_ru` varchar(50) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`order_statuses_id`),
+  UNIQUE KEY `order_status_name` (`order_status_name`),
+  UNIQUE KEY `order_status_name_ru` (`order_status_name_ru`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+CREATE TABLE IF NOT EXISTS `repair_types` (
+  `repair_types_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `name_ru` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `duration` int(2) NOT NULL,
+  `available` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`repair_types_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `users_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(128) NOT NULL, 
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`users_id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=111 ;
+
+CREATE TABLE IF NOT EXISTS `user_authorization` (
+  `user_authorization_id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) NOT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `desc_en` varchar(50) NOT NULL DEFAULT '-',
+  `desc_ru` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '-',
+  PRIMARY KEY (`user_authorization_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=106 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
